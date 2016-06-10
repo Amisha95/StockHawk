8ae172b241bfcd1706fc08684c8434a9b539aecc
m1 <amisha.saya@gmail.com>
2016-06-10 22:40:27 +0530

    Initial commit

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,9 @@
+package com.sam_chordas.android.stockhawk.touch_helper;
+
+/**
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
